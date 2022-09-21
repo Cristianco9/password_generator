8352b9a3afaf96456db0d5b3a2f1ae17c9139a4e
Cristian Camilo <cristian_cortes_ortiz@hotmail.com>
2022-09-21 00:28:11 -0500

    code reviewed and commented

--- password_generator.py
+++ password_generator.py
@@ -1,30 +1,49 @@
+# Shebang
+#!/usr/bin/env python
+
+# Importing 2 modules: 'random' to generete random numbers and 'string' to generete ASCII characters
 import random
 import string
 
+# Password generator function, take the password length defined by the user as parameter
 def password_generator(character_length):
-    character = string.ascii_lowercase + string.digits + string.punctuation + string.ascii_uppercase
 
+    # This variable storage ASCII characters
+    character = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+    
+    # This list saves the characters that make up the password
     password = []
-
+    
+    # This loop iterates storing random characters
     while (len(password) < character_length):
+
+        # The method 'random.choice' allow saves random characters in a variable
         character_storage = random.choice(character)
+        # Add the random characters list to the password list 
         password.append(character_storage)
 
+    # Join all characters in the list in a single string
     password = "".join(password)
+    # As function output return the password list 
     return password
 
+# Main function
 def run():
 
-    print("\n****************************************************")
+    print("****************************************************")
     print("**************** PASSWORD GENERATOR ****************")
     print("****************************************************")
+    # Request password length 
     character_length = int(input("Please define the password length: "))
+    # Run password generator function
     password = password_generator(character_length)
-    print("Your password of {} characters is".format(character_length))
+    # Print user password length
+    print("Your password of {} characters is: ".format(character_length))
+    # print the password
     print(password)
     print("********************** DONE ************************")
     print("****************** SCRIPT ENDED ********************")
     
-
+# Entry point defined
 if __name__ == '__main__':
     run()
